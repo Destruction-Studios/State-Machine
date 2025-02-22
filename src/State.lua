470ac15b995b6786794fe3e583c657bfe0128bbc
Destruction Studios <founderofbeans@gmail.com>
2025-02-22 12:15:55 -0500

    Fix

--- src/State.lua
+++ src/State.lua
@@ -1,4 +1,3 @@
-
 local NO_STATE_ERR = "'%s' does not have a StateMachine"
 local STATE_FUNCTION_PROMISE_ERR = "\n\nError running '%s' function for state '%s'\n\n%s\n\n"
 local NO_STATE_FUNCTION_ERR = "'%s' does not have function '%s'"
@@ -10,104 +9,102 @@ local State = {}
 local StateMT = {}
 StateMT.__index = StateMT
 
-export type StateFunction = (self:typeof(State.new())) -> nil
+export type StateFunction = (self: typeof(State.new())) -> nil
 
-function State.new(name:string)
-    local self = {}
-    setmetatable(self, StateMT)
+function State.new(name: string)
+	local self = {}
+	setmetatable(self, StateMT)
 
-    self._trove = nil
-    self._stateMachine = nil
-    self._name = name or "Unnamed State"
+	self._trove = nil
+	self._stateMachine = nil
+	self._name = name or "Unnamed State"
 
-    return self
+	return self
 end
 
 function StateMT:CreateTrove()
-    self:_clean()
-    self._trove = Trove.new()
-    return self._trove
+	self:_clean()
+	self._trove = Trove.new()
+	return self._trove
 end
 
 function StateMT:GetStateMachine()
-    return self._stateMachine
+	return self._stateMachine
 end
 
 function StateMT:AttachStateMachine(stateMachine)
-    self._stateMachine = stateMachine
+	self._stateMachine = stateMachine
 end
 
 function StateMT:GetName()
-    return self._name
+	return self._name
 end
 
 function StateMT:_clean()
-    if self._trove then
-        self._trove:Clean()
-        self._trove = nil
-    end
+	if self._trove then
+		self._trove:Clean()
+		self._trove = nil
+	end
 end
 
 function StateMT:_machineStart()
-    if not self.MachineStart then
-        return
-    end
-    return Promise.try(self.MachineStart, self):catch(function(err)
-        warn(STATE_FUNCTION_PROMISE_ERR:format("MachineStart", self:GetName(), tostring(err)))
-    end)
+	if not self.MachineStart then
+		return
+	end
+	return Promise.try(self.MachineStart, self):catch(function(err)
+		warn(STATE_FUNCTION_PROMISE_ERR:format("MachineStart", self:GetName(), tostring(err)))
+	end)
 end
 
 function StateMT:_machineStop()
-    if not self.MachineStop then
-        return
-    end
-    return Promise.try(self.MachineStop, self):catch(function(err)
-        warn(STATE_FUNCTION_PROMISE_ERR:format("MachineStop", self:GetName(), tostring(err)))
-    end)
+	if not self.MachineStop then
+		return
+	end
+	return Promise.try(self.MachineStop, self):catch(function(err)
+		warn(STATE_FUNCTION_PROMISE_ERR:format("MachineStop", self:GetName(), tostring(err)))
+	end)
 end
 
 function StateMT:_entered()
-    assert(self:GetStateMachine(), NO_STATE_ERR:format(self:GetName()))
-    assert(typeof(self.Entered) == "function", NO_STATE_FUNCTION_ERR:format(self:GetName(), "Entered"))
+	assert(self:GetStateMachine(), NO_STATE_ERR:format(self:GetName()))
+	assert(typeof(self.Entered) == "function", NO_STATE_FUNCTION_ERR:format(self:GetName(), "Entered"))
 
-    return Promise.try(self.Entered, self):catch(function(err)
-        warn(STATE_FUNCTION_PROMISE_ERR:format("Entered", self:GetName(), tostring(err)))
-    end)
+	return Promise.try(self.Entered, self):catch(function(err)
+		warn(STATE_FUNCTION_PROMISE_ERR:format("Entered", self:GetName(), tostring(err)))
+	end)
 end
 
 function StateMT:_exited()
-    assert(self:GetStateMachine(), NO_STATE_ERR:format(self:GetName()))
-    -- assert(typeof(self.Exited) == "function", NO_STATE_FUNCTION_ERR:format(self:GetName(), "Exited"))
+	assert(self:GetStateMachine(), NO_STATE_ERR:format(self:GetName()))
+	-- assert(typeof(self.Exited) == "function", NO_STATE_FUNCTION_ERR:format(self:GetName(), "Exited"))
 
-    self:_clean()
+	self:_clean()
 
-    if self.Exited == nil then
-        return
-    end
+	if self.Exited == nil then
+		return
+	end
 
-
-    return Promise.try(self.Exited, self):catch(function(err)
-        warn(STATE_FUNCTION_PROMISE_ERR:format("Exited", self:GetName(), tostring(err)))
-    end)
+	return Promise.try(self.Exited, self):catch(function(err)
+		warn(STATE_FUNCTION_PROMISE_ERR:format("Exited", self:GetName(), tostring(err)))
+	end)
 end
 
-
 function StateMT:_cycled()
-    assert(self:GetStateMachine(), NO_STATE_ERR:format(self:GetName()))
-    assert(typeof(self.Cycled) == "function", NO_STATE_FUNCTION_ERR:format(self:GetName(), "Cycled"))
+	assert(self:GetStateMachine(), NO_STATE_ERR:format(self:GetName()))
+	assert(typeof(self.Cycled) == "function", NO_STATE_FUNCTION_ERR:format(self:GetName(), "Cycled"))
 
-    local function setNextState()
-        local stateMachine = self:GetStateMachine()
-        stateMachine:SetNextState(self:GetName())
-    end
+	local function setNextState()
+		local stateMachine = self:GetStateMachine()
+		stateMachine:SetNextState(self:GetName())
+	end
 
-    return Promise.try(self.Cycled, self, setNextState):catch(function(err)
-        warn(STATE_FUNCTION_PROMISE_ERR:format("Cycled", self:GetName(), tostring(err)))
-    end)
+	return Promise.try(self.Cycled, self, setNextState):catch(function(err)
+		warn(STATE_FUNCTION_PROMISE_ERR:format("Cycled", self:GetName(), tostring(err)))
+	end)
 end
 
 function StateMT:Clone()
-    return table.clone(self)
+	return table.clone(self)
 end
 
 return State
--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,3 @@
-
 local State = require(script.State)
 local Signal = require(script.Parent.Signal)
 
@@ -6,44 +5,44 @@ local Signal = require(script.Parent.Signal)
 export type StateIdentifier = string | number
 
 export type TroveLike = {
-    Clean:(self:TroveLike) -> nil,
-    Add:<T>(self:TroveLike, object:T, cleanup:any?) -> T,
+	Clean: (self: TroveLike) -> nil,
+	Add: <T>(self: TroveLike, object: T, cleanup: any?) -> T,
 }
 
 export type State = {
-    GetStateMachine:(self:State) -> StateMachine,
-    GetName:(self:State) -> string,
+	GetStateMachine: (self: State) -> StateMachine,
+	GetName: (self: State) -> string,
 
-    CreateTrove:(self:State) -> TroveLike,
-    
-    AttachStateMachine:(self:State, stateMachine:StateMachine) -> nil,
+	CreateTrove: (self: State) -> TroveLike,
 
-    Cycled:(self:State, enterState: () -> nil) -> nil,
-    Entered:(self:State) -> nil,
-    Exited:(self:State) -> nil,
+	AttachStateMachine: (self: State, stateMachine: StateMachine) -> nil,
 
-    MachineStart:(self:State) -> nil,
-    MachineStop:(self:State) -> nil,
+	Cycled: (self: State, enterState: () -> nil) -> nil,
+	Entered: (self: State) -> nil,
+	Exited: (self: State) -> nil,
 
-    Clone:(self:State) -> State,
+	MachineStart: (self: State) -> nil,
+	MachineStop: (self: State) -> nil,
+
+	Clone: (self: State) -> State,
 }
 
 export type StateMachine = {
-    Debug:(self:StateMachine) -> StateMachine,
+	Debug: (self: StateMachine) -> StateMachine,
 
-    GetState:(self:StateMachine, stateIdentifier:StateIdentifier) -> State,
-    GetStates:(self:StateMachine) -> {[number]: State},
-    GetCurrentState:(self:StateMachine) -> State,
-    GetLastState:(self:StateMachine) -> State,
+	GetState: (self: StateMachine, stateIdentifier: StateIdentifier) -> State,
+	GetStates: (self: StateMachine) -> { [number]: State },
+	GetCurrentState: (self: StateMachine) -> State,
+	GetLastState: (self: StateMachine) -> State,
 
-    SetStates:(self:StateMachine, states:{State}) -> StateMachine,
-    SetDefaultState:(self:StateMachine, defaultState:StateIdentifier) -> StateMachine,
-    SetNextState:(self:StateMachine, nextState:StateIdentifier) -> StateMachine,
+	SetStates: (self: StateMachine, states: { State }) -> StateMachine,
+	SetDefaultState: (self: StateMachine, defaultState: StateIdentifier) -> StateMachine,
+	SetNextState: (self: StateMachine, nextState: StateIdentifier) -> StateMachine,
 
-    UpdateState:(self:StateMachine) -> StateMachine,
-    Start:(self:StateMachine) -> StateMachine,
-    Stop:(self:StateMachine) -> nil,
-    Destroy:(self:StateMachine) -> nil,
+	UpdateState: (self: StateMachine) -> StateMachine,
+	Start: (self: StateMachine) -> StateMachine,
+	Stop: (self: StateMachine) -> nil,
+	Destroy: (self: StateMachine) -> nil,
 }
 
 local NO_STATE_AT_INDEX_ERR = "Could not find state at index '%s'"
@@ -59,194 +58,202 @@ local StateMachineMT = {}
 StateMachineMT.__index = StateMachineMT
 
 StateMachine.State = State :: {
-    new: (name:string) -> State,
+	new: (name: string) -> State,
 }
 
-function StateMachine.new(states:{State}): StateMachine
-    local self = {}
-    setmetatable(self, StateMachineMT)
+function StateMachine.new(states: { State }): StateMachine
+	local self = {}
+	setmetatable(self, StateMachineMT)
 
-    self._states = states
+	self._states = states
 
-    self._stopped = false
+	self._stopped = false
 
-    self._currentState = nil
-    self._defaultState = nil
-    self._queuedState = nil
-    self._lastState = nil
+	self._currentState = nil
+	self._defaultState = nil
+	self._queuedState = nil
+	self._lastState = nil
 
-    self._debug = false
+	self._debug = false
 
-    self.StateChanged = Signal.new()
+	self.StateChanged = Signal.new()
 
-    self._started = false
+	self._started = false
 
-    return self
+	return self
 end
 
 function StateMachineMT:_printDebug(str)
-    if self._debug then
-        print(`\n[STATE-MACHINE] {str}\n`)
-    end
+	if self._debug then
+		print(`\n[STATE-MACHINE] {str}\n`)
+	end
 end
 
 function StateMachineMT:Debug()
-    self._debug = true
+	self._debug = true
 
-    return self
+	return self
 end
 
 function StateMachineMT:GetLastState()
-    return self._lastState
+	return self._lastState
 end
 
-function StateMachineMT:GetStateFromKey(stateKey:StateIdentifier): State
-    local states = self:GetStates()
-    local newState = nil
-    if typeof(stateKey) == "number" then
-        newState = states[stateKey]
-        assert(newState, NO_STATE_AT_INDEX_ERR:format(stateKey))
-    elseif typeof(stateKey) == "string" then
-        for _, state in states do
-            if state:GetName() == stateKey then
-                newState = state
-                break
-            end
-        end
-        assert(newState, NO_STATE_AT_KEY_ERR:format(stateKey))
-    end
+function StateMachineMT:GetStateFromKey(stateKey: StateIdentifier): State
+	local states = self:GetStates()
+	local newState = nil
+	if typeof(stateKey) == "number" then
+		newState = states[stateKey]
+		assert(newState, NO_STATE_AT_INDEX_ERR:format(stateKey))
+	elseif typeof(stateKey) == "string" then
+		for _, state in states do
+			if state:GetName() == stateKey then
+				newState = state
+				break
+			end
+		end
+		assert(newState, NO_STATE_AT_KEY_ERR:format(stateKey))
+	end
 
-    return newState
+	return newState
 end
 
 function StateMachineMT:GetCurrentState(): State
-    return self._currentState
+	return self._currentState
 end
 
-function StateMachineMT:GetStates(): {[number]:State}
-    return self._states
+function StateMachineMT:GetStates(): { [number]: State }
+	return self._states
 end
 
-function StateMachineMT:SetStates(states:{State})
-    assert(typeof(states) == "table", `Invalid type for argument #1 :SetStates expected 'table' got {typeof(states)}`)
+function StateMachineMT:SetStates(states: { State })
+	assert(typeof(states) == "table", `Invalid type for argument #1 :SetStates expected 'table' got {typeof(states)}`)
 
-    local tracked = {}
-    for _, v in states do
-        if table.find(tracked, v:GetName()) then
-            error(DUPE_STATE_NAME:format(v:GetName()))
-        end
-        table.insert(tracked, v:GetName())
-    end
-    table.clear(tracked)
+	local tracked = {}
+	for _, v in states do
+		if table.find(tracked, v:GetName()) then
+			error(DUPE_STATE_NAME:format(v:GetName()))
+		end
+		table.insert(tracked, v:GetName())
+	end
+	table.clear(tracked)
 
-    self._states = states
+	self._states = states
 
-    return self
+	return self
 end
 
-function StateMachineMT:SetDefaultState(defaultState:StateIdentifier)
-    assert(not self._started, ALREADY_STARTED_ERR)
+function StateMachineMT:SetDefaultState(defaultState: StateIdentifier)
+	assert(not self._started, ALREADY_STARTED_ERR)
 
-    self._defaultState = self:GetStateFromKey(defaultState)
+	self._defaultState = self:GetStateFromKey(defaultState)
 
-    return self
+	return self
 end
 
-function StateMachineMT:SetNextState(state:StateIdentifier)
-    assert(self._started, NOT_STARTED_ERR)
-    self._queuedState = self:GetStateFromKey(state)
+function StateMachineMT:SetNextState(state: StateIdentifier)
+	assert(self._started, NOT_STARTED_ERR)
+	self._queuedState = self:GetStateFromKey(state)
 
-    return self
+	return self
 end
 
 function StateMachineMT:_machineStart()
-    for _, v in self:GetStates() do
-        v:_machineStart()
-    end
+	for _, v in self:GetStates() do
+		v:_machineStart()
+	end
 end
 
 function StateMachineMT:_machineStop()
-    for _, v in self:GetStates() do
-        v:_machineStop()
-    end
+	for _, v in self:GetStates() do
+		v:_machineStop()
+	end
 end
 
 function StateMachineMT:UpdateState()
-    local nextState = nil
-    if self._queuedState ~= nil then
-        self:_printDebug(`State Override Found.`)
-        nextState = self._queuedState
-        self._queuedState = nil
-    else
-        for _, v in self:GetStates() do
-            if self._stopped then
-                return
-            end
-            local shouldBreak = v:_cycled():andThen(function()
-                if self._queuedState ~= nil then
-                    return true
-                end
-                return false
-            end):expect()
-            self:_printDebug(`{v:GetName()} Cycled result: {shouldBreak}`)
-            if shouldBreak then
-                nextState = v
-                break
-            end
-        end
-    end
+	local nextState = nil
+	if self._queuedState ~= nil then
+		self:_printDebug(`State Override Found.`)
+		nextState = self._queuedState
+		self._queuedState = nil
+	else
+		for _, v in self:GetStates() do
+			if self._stopped then
+				return
+			end
+			local shouldBreak = v:_cycled()
+				:andThen(function()
+					if self._queuedState ~= nil then
+						return true
+					end
+					return false
+				end)
+				:expect()
+			self:_printDebug(`{v:GetName()} Cycled result: {shouldBreak}`)
+			if shouldBreak then
+				nextState = v
+				break
+			end
+		end
+	end
 
-    if self._stopped then
-        return
-    end
-    
-    self._queuedState = nil
+	if self._stopped then
+		return
+	end
 
-    nextState = nextState or self._defaultState
+	self._queuedState = nil
 
-    self:_printDebug(`Entering new state {nextState:GetName()}`)
+	nextState = nextState or self._defaultState
 
-    self._lastState = self._currentState
+	self:_printDebug(`Entering new state {nextState:GetName()}`)
 
-    self._currentState = nextState
-    self._currentState:_entered()
+	if self._lastState then
+		self._lastState:_exited()
+	end
 
-    return self
+	self._lastState = self._currentState
+
+	self._currentState = nextState
+	self._currentState:_entered()
+
+	return self
 end
 
 function StateMachineMT:Start()
-    assert(not self._started, ALREADY_STARTED_ERR)
-    assert(self._defaultState, NO_DEFAULT_STATE_ERR)
-    assert(self._states ~= nil, NO_STATES_ERR)
-    assert(#self._states > 0, NO_STATES_ERR)
+	assert(not self._started, ALREADY_STARTED_ERR)
+	assert(self._defaultState, NO_DEFAULT_STATE_ERR)
+	assert(self._states ~= nil, NO_STATES_ERR)
+	assert(#self._states > 0, NO_STATES_ERR)
 
-    for _, state in self:GetStates() do
-        state:AttachStateMachine(self)
-    end
+	for _, state in self:GetStates() do
+		state:AttachStateMachine(self)
+	end
 
-    self._queuedState = self._defaultState
-    self._started = true
+	self._queuedState = self._defaultState
+	self._started = true
 
-    self:_machineStart()
-    self:UpdateState()
+	self:_machineStart()
+	self:UpdateState()
 
-    self:_printDebug(`Started State Machine with default state {self._currentState:GetName()}`)
+	self:_printDebug(`Started State Machine with default state {self._currentState:GetName()}`)
 
-    return self
+	return self
 end
 
 function StateMachineMT:Stop()
-    self._stopped = true
+	self._stopped = true
 
-    for _, v in self:GetStates() do
-        v:_clean()
-    end
-    self:GetCurrentState():_exited()
-    self:_machineStop()
+	for _, v in self:GetStates() do
+		v:_clean()
+	end
+	if self._currentState then
+		self:GetCurrentState():_exited()
+	end
+	self:_machineStop()
 end
 
 function StateMachineMT:Destroy()
-    self:Stop()
+	self:Stop()
 end
 
 return StateMachine
